142e54373f5c5c2103418bbd0a542dd177ae1e59
justtinCHOI <nicolao011113@gmail.com>
2024-09-18 13:38:16 +0900

    create file for solveing fibonacci sequence problem

--- python/2_find_consecutive_sum_ways.py
+++ python/2_find_consecutive_sum_ways.py
@@ -2,6 +2,7 @@ def solution(n):
     answer = 0
     k = 1
 
+    # k(k+1)/2 <= n 인 k의 최대값까지만 검사
     while k * (k - 1) // 2 < n:
         kx = n - k * (k - 1) // 2
         if kx > 0 and kx % k == 0:
@@ -9,5 +10,3 @@ def solution(n):
         k += 1
 
     return answer
-
-print(solution(15))
