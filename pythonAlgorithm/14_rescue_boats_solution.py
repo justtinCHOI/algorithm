c2428b938ea7419f1473509257ddc2f8645780fe
justtinCHOI <nicolao011113@gmail.com>
2024-09-26 23:58:42 +0900

    Add python solution for rescuing boats solutions

--- pythonAlgorithm/14_rescue_boats_solution.py
+++ pythonAlgorithm/14_rescue_boats_solution.py
@@ -1,3 +1,17 @@
 def solution(people, limit):
-    answer = 0
-    return answer
+
+    people.sort()
+
+    boats = 0
+    left, right = 0, len(people) - 1
+
+    while left < right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
+        boats += 1
+
+    if left == right:
+        boats += 1
+
+    return boats
